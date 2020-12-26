8d7fc71a2b6b24afd0cd8b0d4f9e30ff800813c8
YukiGasai <r.lindede@googlemail.com>
2020-12-26 15:41:58 +0100

    Better hide of offline members

--- plugins/AvatarChanger.plugin.js
+++ plugins/AvatarChanger.plugin.js
@@ -6,67 +6,70 @@
 
 module.exports = class AvatarChanger {
 
-	getSettingsPanel () {
-		const fs = require('fs'); 
-		const path = require('path');
-		let html = fs.readFileSync( path.join(BdApi.Plugins.folder,'AvatarChanger.settings.html'),'utf8');
+    getSettingsPanel() {
+        const fs = require('fs');
+        const path = require('path');
+        let html = fs.readFileSync(path.join(BdApi.Plugins.folder, 'AvatarChanger.settings.html'), 'utf8');
         return html;
     };
 
-	getName () {return "AvatarChanger";}
+    getName() { return "AvatarChanger"; }
 
-	getDescription () {return "Random Avatar";}
+    getDescription() { return "Random Avatar"; }
 
-	getVersion () {return "1.3.0";}
+    getVersion() { return "1.3.0"; }
 
-	getAuthor () {return "L7Yuki Gasai";}
+    getAuthor() { return "L7Yuki Gasai"; }
 
-	//legacy
-	load () {}
+    //legacy
+    load() {}
 
-	start () {
+    start() {
 
-	const fs = require("fs")
-	const os = require("os");
-	const path = require('path');
-	const cp = require('child_process');
-	const AvatarChangerSettings = {
-		AvatarFolderPath : '',
-		AvatarPathForConsole : '',
-		DiscordConsolePath : '',
-		DiscordToken: ''
-	}
+        const fs = require("fs")
+        const os = require("os");
+        const path = require('path');
+        const cp = require('child_process');
+        const AvatarChangerSettings = {
+            AvatarFolderPath: '',
+            AvatarPathForConsole: '',
+            DiscordConsolePath: '',
+            DiscordToken: ''
+        }
 
-	AvatarChangerSettings.AvatarFolderPath = BdApi.loadData("AvatarChanger", "AvatarPath");
-	if(AvatarChangerSettings.AvatarFolderPath == undefined){
-		return 
-	}
-	AvatarChangerSettings.DiscordConsolePath = BdApi.loadData("AvatarChanger", "ConsolePath");
-	if(AvatarChangerSettings.DiscordConsolePath == undefined){
-		return 
-	}
-	AvatarChangerSettings.DiscordToken = BdApi.loadData("AvatarChanger", "DiscordToken");
-	if(AvatarChangerSettings.DiscordToken == undefined){
-		return 
-	}
+        AvatarChangerSettings.AvatarFolderPath = BdApi.loadData("AvatarChanger", "AvatarPath");
+        if (AvatarChangerSettings.AvatarFolderPath == undefined) {
+            return
+        }
+        AvatarChangerSettings.DiscordConsolePath = BdApi.loadData("AvatarChanger", "ConsolePath");
+        if (AvatarChangerSettings.DiscordConsolePath == undefined) {
+            return
+        }
+        AvatarChangerSettings.DiscordToken = BdApi.loadData("AvatarChanger", "DiscordToken");
+        if (AvatarChangerSettings.DiscordToken == undefined) {
+            return
+        }
 
-	fs.readdir(AvatarChangerSettings.AvatarFolderPath , function (err, Filearray) {
-		if (err) console.log('Unable to scan ' + AvatarChangerSettings.AvatarFolderPath  + " :" + err);
+        fs.readdir(AvatarChangerSettings.AvatarFolderPath, function(err, Filearray) {
+            if (err) console.log('Unable to scan ' + AvatarChangerSettings.AvatarFolderPath + " :" + err);
 
-		var index = Math.floor(Math.random() * Filearray.length);
+            var index = Math.floor(Math.random() * Filearray.length);
 
-		AvatarChangerSettings.AvatarPathForConsole  = path.join(AvatarChangerSettings.AvatarFolderPath , Filearray[index]);
+            AvatarChangerSettings.AvatarPathForConsole = path.join(AvatarChangerSettings.AvatarFolderPath, Filearray[index]);
 
-		if(os.platform="win32")
-		AvatarChangerSettings.AvatarPathForConsole  = AvatarChangerSettings.AvatarPathForConsole.replace(/\\/g,'\\\\');
+            if (os.platform = "win32")
+                AvatarChangerSettings.AvatarPathForConsole = AvatarChangerSettings.AvatarPathForConsole.replace(/\\/g, '\\\\');
 
-		cp.exec(AvatarChangerSettings.DiscordConsolePath + ' -t ' + '"user ' + AvatarChangerSettings.DiscordToken + '"' + ' -x ' + '"avatar ' + AvatarChangerSettings.AvatarPathForConsole + '"' );
+            var Status = BdApi.findModuleByProps("guildPositions").status;
 
-	});
-  }
+            if (Status !== "invisible") {
+                cp.exec(AvatarChangerSettings.DiscordConsolePath + ' -t ' + '"user ' + AvatarChangerSettings.DiscordToken + '"' + ' -x ' + '"avatar ' + AvatarChangerSettings.AvatarPathForConsole + '"');
+            }
+        });
+    }
 
-	initialize(){}
+    initialize() {}
 
-	stop(){}
+    stop() {}
 
 }
--- plugins/CleanUp.plugin.js
+++ plugins/CleanUp.plugin.js
@@ -5,141 +5,152 @@
  */
 var keys = [];
 var Serverlist;
+var showOffline;
 var secbool = true;
 
-   
-function HideServer(){
-    if(secbool){
-        for(var i = 0; i < Serverlist.length; i++){
-            var target = $("[aria-label*='"+Serverlist[i].Name+"']").parent();
-            if(target.parent().parent('.wrapper-25eVIn.da-wrapper').length){
 
-                if(!$("[aria-label*='"+Serverlist[i].Name+"']").parent().is(":hidden"))
-                    $("[aria-label*='"+Serverlist[i].Name+"']").parent().hide();
-                   
+function HideServer() {
+    if (secbool) {
+        for (var i = 0; i < Serverlist.length; i++) {
+            var target = $("[aria-label*='" + Serverlist[i].Name + "']").parent();
+            if (target.parent().parent('.wrapper-25eVIn.da-wrapper').length) {
+
+                $("[aria-label*='" + Serverlist[i].Name + "']").parent().parent().parent().hide();
+
             }
         }
-    }else{
-        for(var i = 0; i < Serverlist.length; i++){
-            var target = $("[aria-label*='"+Serverlist[i].Name+"']").parent();
-            if(target.parent().parent('.wrapper-25eVIn.da-wrapper').length){
-                if($("[aria-label*='"+ Serverlist[i].Name+"']").parent().is(":hidden"))
-                    $("[aria-label*='"+ Serverlist[i].Name+"']").parent().show();
+    } else {
+        for (var i = 0; i < Serverlist.length; i++) {
+            var target = $("[aria-label*='" + Serverlist[i].Name + "']").parent();
+            if (target.parent().parent('.wrapper-25eVIn.da-wrapper').length) {
+                $("[aria-label*='" + Serverlist[i].Name + "']").parent().parent().parent().show();
             }
         }
     }
 }
 
-class CleanUp{
-	constructor () {}
+class CleanUp {
+    constructor() {}
 
-	getName () {return "CleanUp";}
+    getName() { return "CleanUp"; }
 
-	getDescription () {return "CleanUp";}
+    getDescription() { return "CleanUp"; }
 
-	getVersion () {return "1.7.0";}
+    getVersion() { return "1.7.0"; }
 
-	getAuthor () {return "Yuki Gasai";}
-	
-	//legacy
-    load(){}
-    
-    start(){
-        Serverlist = BdApi.loadData("CleanUp","Servers");
-        if(Serverlist == undefined)BdApi.saveData("CleanUp","Servers",[]);
-        Serverlist = BdApi.loadData("CleanUp","Servers");
+    getAuthor() { return "Yuki Gasai"; }
+
+    getSettingsPanel() {
+            const fs = require('fs');
+            const path = require('path');
+            let html = fs.readFileSync(path.join(BdApi.Plugins.folder, 'CleanUp.settings.html'), 'utf8');
+            return html;
+        }
+        //legacy
+    load() {}
+
+    start() {
+        Serverlist = BdApi.loadData("CleanUp", "Servers");
+        if (Serverlist == undefined) BdApi.saveData("CleanUp", "Servers", []);
+        Serverlist = BdApi.loadData("CleanUp", "Servers");
+
+        showOffline = BdApi.loadData("CleanUp", "showOffline");
+        if (showOffline == undefined) BdApi.saveData("CleanUp", "showOffline", false);
+        showOffline = BdApi.loadData("CleanUp", "showOffline");
+
+        if (!showOffline) {
+            BdApi.injectCSS('CleanUpCSS', ".da-offline {display:none;}");
+        } else {
+            BdApi.clearCSS('CleanUpCSS');
+        }
 
 
         function checkFlag() {
-            if($('.listItem-2P_4kh.da-listItem').length) {
-                $(document).off('mousedown').on("keydown",function(e){
-                    keys[e.keyCode] = true; 
+            if ($('.listItem-2P_4kh.da-listItem').length) {
+                $(document).off('mousedown').on("keydown", function(e) {
+                    keys[e.keyCode] = true;
                     // . KEY
                     if (keys[190]) {
-                        console.log("IT happend")
-                        if(secbool)secbool=false;
-                        else secbool = true;
+
+                        secbool = !secbool;
+                        console.log(secbool);
                         HideServer();
                     }
                 });
-                $(document).off('mousedown').on("keyup",function(e){keys[e.keyCode] = false;});
-               HideServer();
+                $(document).off('mousedown').on("keyup", function(e) { keys[e.keyCode] = false; });
+                HideServer();
             }
         }
-        window.setTimeout(checkFlag, 1000);   
-       
+        window.setTimeout(checkFlag, 1000);
+
     }
 
-    onSwitch(){
-       
+    onSwitch() {
+
 
         var Serverdiv = $(".scroller-2TZvBN.da-scroller.none-2Eo-qx.scrollerBase-289Jih").find("[aria-controls='Servers']");
 
         var servers = $(".listItem-2P_4kh.da-listItem");
-        
-        servers.each(function(index){
 
-            if(index > 3){
+        servers.each(function(index) {
 
-            $(this).off('mousedown').on('mousedown', function(e){
-                var Name =  $(this).find(".wrapper-1BJsBx.da-wrapper").attr("aria-label");
-                var found = false;
-                for(var i = 0; i < Serverlist.length; i++) {
-                    if (Serverlist[i].Name == Name) {
-                        found = true;
-                        break;
+            if (index > 3) {
+
+                $(this).off('mousedown').on('mousedown', function(e) {
+                    var Name = $(this).find(".wrapper-1BJsBx.da-wrapper").attr("aria-label");
+                    var found = false;
+                    for (var i = 0; i < Serverlist.length; i++) {
+                        if (Serverlist[i].Name == Name) {
+                            found = true;
+                            break;
+                        }
                     }
-                }
-                console.log("CLICK");
-                //Midddle Mouse 
-                if(e.which == 2){ 
-             
-                    e.preventDefault();
-                    if(found){
-                        BdApi.showConfirmationModal("Show Server "+Name,[],{
-                            danger: true,
-                            confirmText: "Yes",
-                            cancelText: "No",
-                            onConfirm :()=>{
-                                var index = Serverlist.indexOf({"Name": Name});
-                                Serverlist.splice(index,1);
-                                BdApi.saveData("CleanUp","Servers",Serverlist);
-                            }
-                        });   
-                    }else{
-                        BdApi.showConfirmationModal("Hide Server "+Name,[],{
-                        danger: true,
-                        confirmText: "Yes",
-                        cancelText: "No",
-                        onConfirm :()=>{
-                                Serverlist.push({"Name": Name});
-                                BdApi.saveData("CleanUp","Servers",Serverlist);
-                            }
-                        });  
+                    console.log("CLICK");
+                    //Midddle Mouse 
+                    if (e.which == 2) {
+
+                        e.preventDefault();
+                        e.stopPropogation();
+                        if (found) {
+                            BdApi.showConfirmationModal("Show Server " + Name, [], {
+                                danger: true,
+                                confirmText: "Yes",
+                                cancelText: "No",
+                                onConfirm: () => {
+                                    var index = Serverlist.indexOf({ "Name": Name });
+                                    Serverlist.splice(index, 1);
+                                    BdApi.saveData("CleanUp", "Servers", Serverlist);
+                                }
+                            });
+                        } else {
+                            BdApi.showConfirmationModal("Hide Server " + Name, [], {
+                                danger: true,
+                                confirmText: "Yes",
+                                cancelText: "No",
+                                onConfirm: () => {
+                                    Serverlist.push({ "Name": Name });
+                                    BdApi.saveData("CleanUp", "Servers", Serverlist);
+                                }
+                            });
+                        }
+                        HideServer();
                     }
-                    HideServer();
-                }
-            });
+                });
             }
         });
-       
-        
+
+
         HideServer();
 
-        var d =  $('.membersGroup-v9BXpm.da-membersGroup').last().html();
-        if($('.membersGroup-v9BXpm.da-membersGroup').last().nextAll() && d.indexOf("Offline") >= 0){
-            $('.membersGroup-v9BXpm.da-membersGroup').last().nextAll().hide();
-        }
-
-        var ServerError =  $('.anchor-3Z-8Bb.anchorUnderlineOnHover-2ESHQB.guildsError-b7zR5H.da-anchor.da-anchorUnderlineOnHover.da-guildsError');
-        if($(ServerError).is(":visible")){
+        var ServerError = $('.anchor-3Z-8Bb.anchorUnderlineOnHover-2ESHQB.guildsError-b7zR5H.da-anchor.da-anchorUnderlineOnHover.da-guildsError');
+        if ($(ServerError).is(":visible")) {
             $(ServerError).hide();
             console.log("Hide Server Error")
-        } 
+        }
     }
-    
-    stop(){
-        
+
+    stop() {
+
 
 
     }
